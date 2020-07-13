990f66205a4fd932adadfe1d8dd5086ce1d07d32
John Nelson <ceesco53@gmail.com>
2020-07-13 17:10:55 -0400

    update

--- install-concourse.sh
+++ install-concourse.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+echo FOR WORKERS ON microk8s
+echo Add --allow-privileged=true to:
+echo 
+echo #kube-apiserver config
+echo sudo vim /var/snap/microk8s/current/args/kube-apiserver
+echo
 microk8s kubectl create namespace concourse
 microk8s helm3 install concourse concourse/concourse --namespace concourse --values concourse.values.yml
--- install-microk8s.sh
+++ install-microk8s.sh
@@ -1,2 +1,3 @@
 sudo snap install microk8s --classic
-microk8s enable helm3 dns storage ingress
+microk8s enable helm3 dns dashboard storage ingress
+sudo microk8s.kubectl proxy --accept-hosts=.* --address=0.0.0.0 &
